fa3cb17d079eddbe83a9c3ecfe28a4f6af10c918
Nick Franczak <nicholasgfranczak@gmail.com>
2025-05-28 18:17:46 -0400

    added a readme and a just a singular file to give us what we want

--- graph/README.md
+++ graph/README.md
@@ -1,13 +1,17 @@
 ### Instructions
 
-This directory allows us to graph postions, velocities, and accelerations of joints.
-In order to properly run we need to enter the virtual enviroment with ` source graph/bin/activate`
-
-
-here is an example of how to run the files
+This directory allows us to graph per joint the inputs passed into `MoveThroughJointPositions` and how they compare to the inputs given through trajectory generation.
+In order to run we first need to create a virtual environment.
+We recommend using [uv](https://docs.astral.sh/uv/getting-started/installation/)
+Once you have  `uv` installed you need to do
 ```
-❯ python3 graph_desired.py [your absolute path here]_trajectory.csv
+uv venv -p python3.10 graph
+source graph/bin/activate
+uv pip install matplotlib numpy pandas
+```
 
+here is an example of how to run the file
+```
+python3 graph.py /home/nick/.viam/module-data/[part-id]/universal-robots-executable/1748360427793_trajectory.csv /home/nick/.viam/module-data/[part-id]/universal-robots-executable/1748360427793_waypoints.csv
 
-❯ python3 graph_actual.py [your absolute path here]_arm_joint_positions.csv
 ```
---
+++ graph/graph.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+def plot_joint_positions(time_of_traj_gen, traj_gen_joint_information, motion_time, motions_positions, joint_idx, filename):
+    plt.figure(figsize=(20, 16))
+    plt.plot(motion_time, motions_positions, label='motion plan', linestyle='--')
+    plt.plot(time_of_traj_gen, traj_gen_joint_information, label='traj gen', linewidth=2)
+    plt.title(f'Joint {joint_idx} Position over Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Position (rad)')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+if len(sys.argv) != 3:
+    print(f"Usage: python {sys.argv[0]} path/to/desired.csv path/to/actual.csv")
+    sys.exit(1)
+
+# the first argument is a path leading to *trajectory.csv <- this is what trajectory generation returns 
+# the second argument is a path leading to *waypoints.csv <- this is what motion planning returns
+traj_gen = sys.argv[1]
+motion = sys.argv[2]
+
+if not os.path.isfile(traj_gen):
+    print(f"Error: File '{traj_gen}' does not exist.")
+    sys.exit(1)
+
+if not os.path.isfile(motion):
+    print(f"Error: File '{motion}' does not exist.")
+    sys.exit(1)
+
+# load trajectory generation data
+df_traj_gen = pd.read_csv(traj_gen)
+# time in this file is relative so we need to add it all up
+df_traj_gen['time_cumulative'] = df_traj_gen['t(s)'].cumsum()
+time_of_traj_gen = df_traj_gen['time_cumulative'].to_numpy()
+
+# load motion data (inputs passed into MoveThroughJointPositions)
+motion_data = np.loadtxt(motion, delimiter=',')
+
+# induce a metric of time into the motion data (linearly spaced to match desired time span)
+motion_time = np.linspace(time_of_traj_gen[0], time_of_traj_gen[-1], motion_data.shape[0])
+
+for i in range(6):
+    traj_gen_joint_information = df_traj_gen[f'j{i}'].to_numpy()
+    motions_positions = motion_data[:, i]
+    filename = f'joint_{i}_position_comparison.png'
+    plot_joint_positions(time_of_traj_gen, traj_gen_joint_information, motion_time, motions_positions, i, filename)
