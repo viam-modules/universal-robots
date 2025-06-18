7b844fee6c238c85eadbb813fa5f7c2f6623b875
Matthew J Perez <matt@mperez.io>
2025-06-18 18:08:32 -0400

    fix

--- src/viam/ur/module/ur_arm.cpp
+++ src/viam/ur/module/ur_arm.cpp
@@ -642,6 +642,8 @@ void URArm::move(std::vector<Eigen::VectorXd> waypoints, std::chrono::millisecon
             throw std::runtime_error("send_trajectory failed");
         };
 
+        std::ofstream of(arm_joint_positions_filename(path, unix_time_ms.count()));
+
         of << "time_ms,read_attempt,joint_0_rad,joint_1_rad,joint_2_rad,joint_3_rad,joint_4_rad,joint_5_rad,joint_0_v,joint_1_v,joint_2_v,"
               "joint_3_v,joint_4_v,joint_5_v\n";
         unsigned attempt = 0;
