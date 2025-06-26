65990396dc05395806c9cd53631caae7efdeb5ee
JohnN193 <92045055+JohnN193@users.noreply.github.com>
2025-06-26 09:48:08 -0400

    add ticket number

--- src/viam/ur/module/ur_arm.cpp
+++ src/viam/ur/module/ur_arm.cpp
@@ -755,7 +755,7 @@ void URArm::move_(std::vector<Eigen::VectorXd> waypoints, std::chrono::milliseco
         of.close();
         VIAM_SDK_LOG(info) << "move: end unix_time " << unix_time.count();
 
-        // TODO: renable this once zero velocity errors are resolved
+        // TODO(RSDK-11063): renable this once zero velocity errors are resolved
         // https://viam.atlassian.net/browse/RSDK-11063
         // if (current_state_->trajectory_status.load() == TrajectoryStatus::k_cancelled) {
         //     throw std::runtime_error("arm's current trajectory cancelled by code");
