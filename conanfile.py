ee522e6ac9197b391c479ff79ef993fa2b915de1
Andrew Morrow <andrew.morrow@viam.com>
2025-10-02 11:55:49 -0400

    [RSDK-10874] Newest eigen release doesn't work, bump min, constrain max

--- conanfile.py
+++ conanfile.py
@@ -35,7 +35,7 @@ class universal_robots(ConanFile):
         # NOTE: If you update the `viam-cpp-sdk` dependency here, it
         # should also be updated in `bin/setup.sh`.
         self.requires("viam-cpp-sdk/[>=0.20.0]")
-        self.requires("eigen/[>=3.3]")
+        self.requires("eigen/[>=3.4 <5.0]")
         self.requires("boost/[>=1.74.0]")
 
     def validate(self):
