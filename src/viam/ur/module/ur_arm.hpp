59ef09eddd122702a3284fafd271070ccc9a3c7e
Nick Franczak <46872531+nfranczak@users.noreply.github.com>
2025-11-14 14:07:43 -0500

    RSDK-12185 - Capture poses arm visits (#124)

--- src/viam/ur/module/ur_arm.hpp
+++ src/viam/ur/module/ur_arm.hpp
@@ -52,11 +52,12 @@ void sampling_func(std::vector<trajectory_sample_point>& samples, double duratio
 }
 
 void write_trajectory_to_file(const std::string& filepath, const std::vector<trajectory_sample_point>& samples);
+void write_pose_to_file(const std::string& filepath, const pose_sample& sample);
 void write_waypoints_to_csv(const std::string& filepath, const std::list<Eigen::VectorXd>& waypoints);
 std::string waypoints_filename(const std::string& path, const std::string& unix_time);
 std::string trajectory_filename(const std::string& path, const std::string& unix_time);
 std::string arm_joint_positions_filename(const std::string& path, const std::string& unix_time);
-std::string move_to_position_trajectory_filename(const std::string& path, const std::string& unix_time);
+std::string move_to_position_pose_filename(const std::string& path, const std::string& unix_time);
 std::string unix_time_iso8601();
 
 class URArm final : public Arm, public Reconfigurable {
