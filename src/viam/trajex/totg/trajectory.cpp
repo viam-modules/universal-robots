8fc1b6a86edccd4e0c0c96acba80384e9c09a937
Nick Franczak <nicholasgfranczak@gmail.com>
2025-12-04 16:50:24 -0500

    appease CI

--- src/viam/trajex/totg/trajectory.cpp
+++ src/viam/trajex/totg/trajectory.cpp
@@ -246,7 +246,7 @@ enum class integration_event : std::uint8_t {
     // Check that the step makes sufficient progress in the direction indicated by dt's sign.
     // For positive dt (forward integration), s_new - s should be >= epsilon.
     // For negative dt (backward integration), s_new - s should be >= -epsilon (i.e., |s_new - s| >= epsilon).
-    const double dt_sign = (dt > 0.0) ? 1.0 : ((dt < 0.0) ? -1.0 : 0.0);
+    const double dt_sign = std::copysign(1.0, dt);
     if ((s_new - s) < arc_length{epsilon * dt_sign}) [[unlikely]] {
         throw std::runtime_error{"Euler step will not make sufficient forward progress - the change in s_new relative to s was too small"};
     }
